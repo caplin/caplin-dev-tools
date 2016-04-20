5db99b2ca613e40b95f3ede4f0680ffba669e295
Brian Di Palma <offler@gmail.com>
2016-04-20 17:00:48 +0100

    feat(xml-loader): Create an XML loader that returns the XML document
    The XML loader listed in the webpack list of loaders returns JSON.

---
+++ xml-loader/index.js
@@ -0,0 +1,13 @@
+
+module.exports = function patchLoader(xmlText) {
+	this.cacheable();
+
+	// We need to turn the XML file into one long string as leaving it raw leads to JS parsing errors.
+	const flattenedXMLText = xmlText.replace(/\n/g, '');
+
+	return `
+	var domParser = new DOMParser();
+	var dom = domParser.parseFromString('${flattenedXMLText}', 'text/xml');
+
+	module.exports = dom.documentElement`;
+};
