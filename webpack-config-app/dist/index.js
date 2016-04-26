342893a0ffffe289b3ef3e1f34706f260642d183
Brian Di Palma <offler@gmail.com>
2016-04-26 15:58:22 +0100

    chore(webpack-config): Update version

--- webpack-config-app/dist/index.js
+++ webpack-config-app/dist/index.js
@@ -168,6 +168,34 @@ var webpackConfigGenerator = function webpackConfigGenerator(argsMap) {
 		}));
 	}
 
+	// Add aliases for the app's libs.
+	var libsDir = (0, _path.resolve)(basePath, 'libs');
+
+	var _iteratorNormalCompletion2 = true;
+	var _didIteratorError2 = false;
+	var _iteratorError2 = undefined;
+
+	try {
+		for (var _iterator2 = (0, _fs.readdirSync)(libsDir)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+			var libDir = _step2.value;
+
+			webpackConfig.resolve.alias[libDir] = (0, _path.resolve)(libsDir, libDir);
+		}
+	} catch (err) {
+		_didIteratorError2 = true;
+		_iteratorError2 = err;
+	} finally {
+		try {
+			if (!_iteratorNormalCompletion2 && _iterator2.return) {
+				_iterator2.return();
+			}
+		} finally {
+			if (_didIteratorError2) {
+				throw _iteratorError2;
+			}
+		}
+	}
+
 	return webpackConfig;
 };
 
