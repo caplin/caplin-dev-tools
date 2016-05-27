a8cfbc99f18323b13e3540334316eca28ae707d4
Brian Di Palma <offler@gmail.com>
2016-05-27 12:43:00 +0100

    feat: Log error if the version of node is not supported

--- brjs-app-converter/src/cli.js
+++ brjs-app-converter/src/cli.js
@@ -1,15 +1,46 @@
 #! /usr/bin/env node
 
+/* eslint-disable no-var */
+
+var path = require('path');
+
+var check = require('check-node-version');
+var parseArgs = require('minimist');
+
+var packageJson = require(path.join(__dirname, '..', 'package.json'));
+
+var options = {
+	node: packageJson.engines.node
+};
+
+function versionCheckCallback(versionError, result) {
+	var errorMessage = 'Not compatible with current node version, please update!';
+
+	if (result.nodeSatisfied) {
+		convertApplication();
+	} else {
+		if (versionError) {
+			errorMessage = versionError.message;
+		}
+
+		console.error(errorMessage); // eslint-disable-line
+		process.exit(1); // eslint-disable-line
+	}
+}
+
+check(options, versionCheckCallback);
+
 // Can't rely on `babel-node` being globally installed so use the register hook instead.
 // This allows the use of ES2015 in the CLI scripts.
 require('babel-register')({
 	// Compile `node_modules` as when the tool is installed it's located in `node_modules`.
 	ignore: false
 });
-const minimist = require('minimist');
 
-const convertor = require('./converter').default;
+function convertApplication() {
+	var convertor = require('./converter').default; // eslint-disable-line
 
-const argv = minimist(process.argv.slice(2));
+	var argv = parseArgs(process.argv.slice(2));
 
-convertor(argv);
+	convertor(argv);
+}
