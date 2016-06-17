63257eafdf9bd672fac8595a1b786304e71e93f1
Dominic Gannaway <dominic.gannaway@caplin.com>
2016-06-17 17:21:49 +0100

    Update CLI to have a build that works, also added try catch to caplin-build-tasks for when copying of files that might not exist

--- build-tasks/build.js
+++ build-tasks/build.js
@@ -41,8 +41,13 @@ function webpackBuildCallback(error, {buildCallback = NO_OP, indexPage, version,
 		const variant = parseArgs(process.argv.slice(2)).variant;
 		const indexFile = indexPage({variant, version});
 
-		copySync(join(process.cwd(), 'scripts', 'WEB-INF'), join(distDir, 'WEB-INF'));
-		copySync(join(process.cwd(), 'public', 'dev'), join(distDir, 'public', version));
+		try {
+			copySync(join(process.cwd(), 'scripts', 'WEB-INF'), join(distDir, 'WEB-INF'));
+			copySync(join(process.cwd(), 'public', 'dev'), join(distDir, 'public', version));	
+		} catch () {
+			// do nothing
+		}
+		
 		writeFileSync(join(distDir, 'index.html'), indexFile, 'utf8');
 		buildCallback();
 
--- cli/templates/app-blank/scripts/build.js
+++ cli/templates/app-blank/scripts/build.js
@@ -2,19 +2,20 @@ import getFullVersion from '@caplin/versioning';
 import {cleanDistAndBuildWAR} from '@caplin/build-tasks';
 import config from '../webpack.config';
 import {readFileSync} from 'fs';
+import {join} from 'path';
 
 function buildCallback() {
 	// add your own callback here
 }
 
 function getIndexPage() {
-	return readFileSync('../index.html', 'utf8');
+	return readFileSync(join(__dirname, '..', 'index.html'), 'utf8');
 }
 
 function versionCalculatedCallback(version) {
 	cleanDistAndBuildWAR({
 		buildCallback,
-		indexPage: getIndexPage(),
+		indexPage: getIndexPage,
 		version,
 		warName: 'untitled',
 		webpackConfig: config
