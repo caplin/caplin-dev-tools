94c610ade43176b714a558c5e2b4b19a9f44cf96
Brian Di Palma <offler@gmail.com>
2016-06-17 10:11:10 +0100

    refactor: Import spacing

--- brjs-app-converter/src/convert-app.js
+++ brjs-app-converter/src/convert-app.js
@@ -1,7 +1,13 @@
-import {readdirSync} from 'fs-extra';
+import {
+	readdirSync
+} from 'fs-extra';
 
-import {moveAspectCode} from './convert-aspect';
-import {moveBladesetCode} from './convert-bladeset';
+import {
+	moveAspectCode
+} from './convert-aspect';
+import {
+	moveBladesetCode
+} from './convert-bladeset';
 
 // Move all code in bladesets/blades/aspects into the packages directory.
 export function moveBRJSApplicationCodeToPackages(conversionMetadata) {
