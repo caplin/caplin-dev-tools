1b0c7fd4773d11c3a91743cce11f84c45ba995ec
Brian Di Palma <offler@gmail.com>
2016-07-04 09:39:51 +0100

    fix(xml-loader): Load documents containing single quotes
    Single quotes can be present in comments, these needed to be escaped.
    
    closes: CTNXT-249

--- xml-loader/index.js
+++ xml-loader/index.js
@@ -2,8 +2,12 @@
 module.exports = function patchLoader(xmlText) {
 	this.cacheable();
 
-	// We need to turn the XML file into one long string as leaving it raw leads to JS parsing errors.
-	const flattenedXMLText = xmlText.replace(/[\r\n]/g, '');
+	const flattenedXMLText = xmlText
+		// We need to turn the XML file into one long string as leaving it raw leads to JS parsing errors.
+		.replace(/[\r\n]/g, '')
+		// We need to escape any single quotes `'` in XML comments as otherwise they would terminate
+		// the `flattenedXMLText` string prematurely.
+		.replace(/'/g, "\\'");
 
 	return `
 	var domParser = new DOMParser();
