0c67112c61e9cb478e24d28cbb7d7d6728d9874b
Brian Di Palma <offler@gmail.com>
2016-08-10 17:24:28 +0100

    docs(service-loader): Add comment explaining webpack module deletion code
    closes: CTNXT-407

--- service-loader/cache-deletion-loader.js
+++ service-loader/cache-deletion-loader.js
@@ -5,6 +5,8 @@ module.exports = function serviceLoader() {
 
 	const serviceRequireModule = generateServiceRequireModule(this._module.rawRequest);
 
+	// Delete the module object from webpack's module cache so each `require('service!alias-name')` goes to the
+	// `ServiceRegistry` afresh. This is necessary in tests where the services can be replaced by different stubs.
 	return `${serviceRequireModule}
 	delete __webpack_require__.c[module.id];`;
 };
