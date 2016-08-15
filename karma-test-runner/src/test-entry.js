74b1a95c7edb3d2ea4c1b22cbd87a0b920e1afc1
Brian Di Palma <offler@gmail.com>
2016-08-15 09:53:25 +0100

    fix(karma-test-runner): Only run UTs
    Running ATs and UTs in the same browser instance is causing env issues

--- karma-test-runner/src/test-entry.js
+++ karma-test-runner/src/test-entry.js
@@ -1,4 +1,4 @@
 
-const testsContext = require.context(PACKAGE_DIRECTORY, true, /.*_test-[ua]t.*\.js$/);
+const testsContext = require.context(PACKAGE_DIRECTORY, true, /.*_test-ut.*\.js$/);
 
 testsContext.keys().forEach(testsContext);
