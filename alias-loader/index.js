b4becb816acac4a4c294b2005dbb58535582606c
Brian Di Palma <offler@gmail.com>
2016-08-24 15:33:51 +0100

    feat(alias-loader): Create module for all aliases to use
    closes: CTNXT-460

--- alias-loader/index.js
+++ alias-loader/index.js
@@ -1,15 +1,13 @@
+
 module.exports = function aliasLoader(aliasExporterSource) {
 	this.cacheable();
 
 	// The `rawRequest` is in the form `alias!caplin.fx.tenor.currency-tenors` so we extract the alias name.
 	const requiredAlias = this._module.rawRequest.replace('alias!', '');
-	const valueToInject = `require('br/AliasRegistry').getClass('${requiredAlias}')`;
 
-	// The alias exporter module has a replaceable placeholder used to specify the alias to
-	// retrieve from the `AliasRegistry`.
-	return aliasExporterSource
-		.replace(
-			/loaderInjectedValue/,
-			valueToInject
-		);
+	if (requiredAlias === '$aliases-data') {
+		return aliasExporterSource;
+	}
+
+	return `module.exports = require('br/AliasRegistry').getClass('${requiredAlias}')`;
 };
