e4e35641081b9c6d4c6c27e6ef02869ac1790a8a
Brian Di Palma <offler@gmail.com>
2016-10-11 12:08:26 +0100

    fix(html-loader): Set the loader as cacheable

--- html-loader/index.js
+++ html-loader/index.js
@@ -1,5 +1,7 @@
 
 module.exports = function htmlLoader(htmlSource) {
+	this.cacheable();
+
 	// Escape newlines, quotes etc that would cause errors with subsequent parsing of the loader return value.
 	const jsonStringHTMLTemplate = JSON.stringify(htmlSource);
 
