81cfd62fb360f88ac9ed5ae1863da25c66a32f90
Brian Di Palma <offler@gmail.com>
2017-01-20 15:38:35 +0000

    fix(webpack-config): Log module requested by getClass

--- webpack-config-app/null.js
+++ webpack-config-app/null.js
@@ -1,4 +1,4 @@
 
-module.exports = function nullModule() {
-	throw new Error('Null module called ' + arguments);
+module.exports = function nullModule(moduleName) {
+	throw new Error('Null module called requesting ' + moduleName);
 };
