6ca91044f85130b0e56caf98be0e109aa6caf2d7
Brian Di Palma <offler@gmail.com>
2017-03-03 16:49:52 +0000

    refactor(cli): Format CLI with Prettier

--- cli/cli.js
+++ cli/cli.js
@@ -4,88 +4,86 @@
  * @module cli
  */
 
-var inquirer = require('inquirer');
+var inquirer = require("inquirer");
 
-var orderedCommands = require('./orderedCommands');
+var orderedCommands = require("./orderedCommands");
 
 /**
  * Immediately runs the provided command with the chosen command options
  *
- * @param commandAndOptionList An array containing the chosen command object at position 0
- * followed by the command options
+ * @param commandAndOptionList An array containing the chosen command object at
+ * position 0 followed by the command options
  */
 var runCommand = function(commandAndOptionList) {
-    var chosenCommand = commandAndOptionList[0];
-    var chosenCommandOptions = commandAndOptionList.splice(1, commandAndOptionList.length);
-    chosenCommand.commandFunction(chosenCommandOptions);
+  var chosenCommand = commandAndOptionList[0];
+  var chosenCommandOptions = commandAndOptionList.splice(
+    1,
+    commandAndOptionList.length
+  );
+  chosenCommand.commandFunction(chosenCommandOptions);
 };
 
 /**
- * Given a validated command, resolves a promise immediately if there are no options. If there are options, returns
- * promise that will resolve once all valid options have been provided by the user
+ * Given a validated command, resolves a promise immediately if there are no 
+ * options. If there are options, returns promise that will resolve once all 
+ * valid options have been provided by the user
  *
- * @param args The command that the user has chosen to run and the current list of command option values
+ * @param args The command that the user has chosen to run and the current list 
+ * of command option values
  *
- * @returns {Promise} Returns a promise that will be resolved once valid option values have been sequentially
- * provided for all options
+ * @returns {Promise} Returns a promise that will be resolved once valid option 
+ * values have been sequentially provided for all options
  */
 var getAllOptionValues = function(args) {
-    var chosenCommand = args[0];
-    var chosenCommandOptions = chosenCommand.options || [];
-    var chosenCommandOptionValues = args[1];
-    var validCommandOptionValues = [];
-    var questionsToAsk = [];
+  var chosenCommand = args[0];
+  var chosenCommandOptions = chosenCommand.options || [];
+  var chosenCommandOptionValues = args[1];
+  var validCommandOptionValues = [];
+  var questionsToAsk = [];
 
-    chosenCommandOptions.forEach((function(commandOption, optionIndex) {
+  chosenCommandOptions.forEach(function(commandOption, optionIndex) {
+    var commandQuestion = commandOption.question;
+    var currentOptionValue = chosenCommandOptionValues[optionIndex];
+    var validate = commandOption.question.validate;
 
-        var commandQuestion = commandOption.question;
-        var currentOptionValue = chosenCommandOptionValues[optionIndex];
-        var validate = commandOption.question.validate;
+    if (validate && validate(currentOptionValue) !== true) {
+      if (validate && currentOptionValue) {
+        console.log(validate(currentOptionValue));
+      }
 
-        if (validate && validate(currentOptionValue) !== true) {
-
-            if (validate && currentOptionValue) {
-                console.log(validate(currentOptionValue));
-            };
-
-            questionsToAsk.push(commandQuestion);
-
-        } else {
-            validCommandOptionValues.push(currentOptionValue);
-        }
-
-    }));
-
-
-    if (questionsToAsk.length > 0) {
-
-        return new Promise(function(resolve, reject) {
-
-            inquirer.prompt(questionsToAsk).then(function(answersObject) {
-
-                var answers = questionsToAsk.map(function(question, index) {
-                   return answersObject[question.name]
-                });
-
-                resolve([chosenCommand].concat(validCommandOptionValues).concat(answers));
-
-            })
-            .catch(function(e) {
-                console.log(e);
-            });
-
-        });
-
+      questionsToAsk.push(commandQuestion);
     } else {
-        return Promise.resolve([chosenCommand].concat(validCommandOptionValues));
+      validCommandOptionValues.push(currentOptionValue);
     }
+  });
 
+  if (questionsToAsk.length > 0) {
+    return new Promise(function(resolve, reject) {
+      inquirer
+        .prompt(questionsToAsk)
+        .then(function(answersObject) {
+          var answers = questionsToAsk.map(function(question, index) {
+            return answersObject[question.name];
+          });
+
+          resolve(
+            [chosenCommand].concat(validCommandOptionValues).concat(answers)
+          );
+        })
+        .catch(function(e) {
+          console.log(e);
+        });
+    });
+  } else {
+    return Promise.resolve([chosenCommand].concat(validCommandOptionValues));
+  }
 };
 
 /**
- * Given a command name, finds the associated command, validates the current working directory and resolves the returned
- * promise. If the command is invalid, ask the user which command they want to run. If the working directory is not
- * valid for the chosen command throw an error
+ * Given a command name, finds the associated command, validates the current 
+ * working directory and resolves the returned promise. If the command is
+ * invalid, ask the user which command they want to run. If the working
+ * directory is not valid for the chosen command throw an error
  *
  * @param chosenCommandName The name of the chosen command
  * @param chosenCommandOptions The current array of option values
@@ -93,67 +91,74 @@ var getAllOptionValues = function(args) {
  * @return {Promise} Resolves once a valid command has been run
  */
 var getChosenCommand = function(chosenCommandName, chosenCommandOptionValues) {
+  return new Promise(function(resolve, reject) {
+    var chosenCommand = orderedCommands.filter(function(command) {
+      return command.name == chosenCommandName;
+    })[0];
 
-    return new Promise(function(resolve, reject) {
+    if (!chosenCommand) {
+      inquirer
+        .prompt(getCommandQuestion(chosenCommandName))
+        .then(function(answer) {
+          chosenCommand = orderedCommands.filter(function(command) {
+            return command.name == answer.commandName;
+          })[0];
 
-        var chosenCommand = orderedCommands.filter(function(command) {
-            return command.name == chosenCommandName
-        })[0];
-
-        if (!chosenCommand) {
-
-            inquirer.prompt(getCommandQuestion(chosenCommandName))
-                .then(function (answer) {
-
-                    chosenCommand = orderedCommands.filter(function(command) {
-                        return command.name == answer.commandName
-                    })[0];
-
-                    if (chosenCommand.isValidWorkingDirectory && chosenCommand.isValidWorkingDirectory(process.cwd()) !== true) {
-                        throw new Error(chosenCommand.isValidWorkingDirectory(process.cwd()));
-                    } else {
-                        resolve([chosenCommand, chosenCommandOptionValues]);
-                    }
-
-                })
-                .catch(function(e) {
-                    console.log(e);
-                });
-
-        } else {
-
-            if (chosenCommand.isValidWorkingDirectory && chosenCommand.isValidWorkingDirectory(process.cwd()) !== true) {
-                throw new Error(chosenCommand.isValidWorkingDirectory(process.cwd()));
-            } else {
-                resolve([chosenCommand, chosenCommandOptionValues]);
-            }
-
-        }
-
-    });
-
+          if (
+            chosenCommand.isValidWorkingDirectory &&
+            chosenCommand.isValidWorkingDirectory(process.cwd()) !== true
+          ) {
+            throw new Error(
+              chosenCommand.isValidWorkingDirectory(process.cwd())
+            );
+          } else {
+            resolve([chosenCommand, chosenCommandOptionValues]);
+          }
+        })
+        .catch(function(e) {
+          console.log(e);
+        });
+    } else {
+      if (
+        chosenCommand.isValidWorkingDirectory &&
+        chosenCommand.isValidWorkingDirectory(process.cwd()) !== true
+      ) {
+        throw new Error(chosenCommand.isValidWorkingDirectory(process.cwd()));
+      } else {
+        resolve([chosenCommand, chosenCommandOptionValues]);
+      }
+    }
+  });
 };
 
 /**
- * Give a command name, generate a question using inquirer that can prompt the user for a valid command
+ * Give a command name, generate a question using inquirer that can prompt the 
+ * user for a valid command
  *
  * @param commandName The currently selected command
  *
- * @returns {InquirerQuestion} The question object that can be passed to inquirer
+ * @returns {InquirerQuestion} The question object that can be passed to 
+ * inquirer
  */
 var getCommandQuestion = function(commandName) {
-    return {
-        type: 'list',
-        name: 'commandName',
-        message: commandName ?
-        'Command "' + commandName + '" not found, what command do you want to run:' :
-            'What command do you want to run:',
-        choices: orderedCommands.map(function(command) {return command.name})
-    };
-}
+  return {
+    type: "list",
+    name: "commandName",
+    message: (
+      commandName
+        ? 'Command "' +
+            commandName +
+            '" not found, what command do you want to run:'
+        : "What command do you want to run:"
+    ),
+    choices: orderedCommands.map(function(command) {
+      return command.name;
+    })
+  };
+};
 
 module.exports = {
-    getChosenCommand: getChosenCommand,
-    getAllOptionValues: getAllOptionValues,
-    runCommand: runCommand
-}
+  getChosenCommand: getChosenCommand,
+  getAllOptionValues: getAllOptionValues,
+  runCommand: runCommand
+};
--- cli/commands/create-app.js
+++ cli/commands/create-app.js
@@ -1,64 +1,71 @@
-var path = require('path');
-var chalk = require('chalk');
-var fs = require('fs');
-var copyTemplate = require('../utils/copyTemplate');
+var path = require("path");
+var chalk = require("chalk");
+var fs = require("fs");
+var copyTemplate = require("../utils/copyTemplate");
 
 var workingDirectoryErrorMessage = function(dir) {
-	return `'${dir}' directory not found.\n\nPlease ensure you are in the project root and you have run the ${chalk.blue('caplin-cli init')} command'`;
-}
+  return `'${dir}' directory not found.
+
+  Please ensure you are in the project root and you have run the ${chalk.blue(
+    "caplin-cli init"
+  )} command'`;
+};
 
 module.exports = {
+  name: "create-app",
 
-	name: "create-app",
+  priority: 10,
 
-	priority: 10,
+  options: [
+    {
+      name: "name",
+      question: {
+        type: "input",
+        name: "value",
+        message: "What do you want to name your app:",
+        validate: function(name) {
+          if (name !== "" && name !== null && name !== undefined) {
+            return true;
+          } else {
+            return "Name must not be blank";
+          }
+        }
+      }
+    }
+  ],
 
-	options: [
-		{
-			name: 'name',
-			question: {
-				type: 'input',
-				name: 'value',
-				message: 'What do you want to name your app:',
-				validate: function(name) {
+  isValidWorkingDirectory: function(dir) {
+    var appsFound = false, packagesFound = false;
 
-					if (name !== "" && name !== null && name !== undefined) {
-						return true;
-					} else {
-						return "Name must not be blank"
-					}
-				}
-			}
-		}
-	],
+    try {
+      appsFound = fs.lstatSync(path.join(process.cwd(), "apps")).isDirectory();
+      packagesFound = fs
+        .lstatSync(path.join(process.cwd(), "caplin-packages"))
+        .isDirectory();
+    } catch (e) {
+    }
 
-	isValidWorkingDirectory: function(dir) {
-		var appsFound = false,
-		 	packagesFound = false;
+    if (!appsFound) return workingDirectoryErrorMessage("apps");
+    if (!packagesFound) return workingDirectoryErrorMessage("caplin-packages");
 
-		try {
-			appsFound = fs.lstatSync(path.join(process.cwd(), 'apps')).isDirectory();
-			packagesFound = fs.lstatSync(path.join(process.cwd(), 'caplin-packages')).isDirectory();
-		} catch(e) {}
+    return true;
+  },
 
-		if (!appsFound) return workingDirectoryErrorMessage('apps');
-		if (!packagesFound) return workingDirectoryErrorMessage('caplin-packages');
-		
-		return true;
-	},
+  commandFunction: function(options) {
+    var name = options[0];
+    var templateId = "app-blank";
 
-	commandFunction: function(options) {
-		var name = options[0];
-		var templateId = "app-blank";
-		
-		copyTemplate(templateId, path.join(process.cwd(), 'apps', name), {
-			appName: name
-		})
-		.then(function() {
-			console.log('\tNew app \'' + name + '\' created!' );
-			console.log('\n\n\tNow cd into apps/' + name + ' and run ' + chalk.blue('npm install') + '\n' );
-		});
-
-	}
-
+    copyTemplate(templateId, path.join(process.cwd(), "apps", name), {
+      appName: name
+    }).then(function() {
+      console.log("\tNew app '" + name + "' created!");
+      console.log(
+        "\n\n\tNow cd into apps/" +
+          name +
+          " and run " +
+          chalk.blue("npm install") +
+          "\n"
+      );
+    });
+  }
 };
