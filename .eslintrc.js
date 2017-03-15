0054da1dcb0208ae0313857dd414f4e03ac17424
Brian Di Palma <offler@gmail.com>
2017-03-15 09:12:37 +0000

    chore(lint): Disable console warnings
    console is easy to use for the simple node scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ const [_, ...restricted] = baseRules.rules["no-restricted-syntax"];
 module.exports = {
   extends: ["airbnb", "prettier"],
   rules: {
+    // The console is the easiest way to provide feedback when executing in node
+    "no-console": 0,
     // Let's allow for..in https://github.com/airbnb/javascript/issues/1122
     "no-restricted-syntax": [
       2,
