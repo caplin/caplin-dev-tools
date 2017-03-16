108db86616da5e343807f70b0dd1c715122b5245
Brian Di Palma <offler@gmail.com>
2017-03-16 17:22:03 +0000

    refactor(brjs-converter): Format with prettier

--- brjs-app-converter/src/create-applications.js
+++ brjs-app-converter/src/create-applications.js
@@ -1,25 +1,32 @@
-import { join } from 'path';
+import { join } from "path";
 
-import { copySync, existsSync, readdirSync, readJsonSync, statSync, writeJsonSync } from 'fs-extra';
+import {
+  copySync,
+  existsSync,
+  readdirSync,
+  readJsonSync,
+  statSync,
+  writeJsonSync
+} from "fs-extra";
 
-import { templateDir } from './converter-data';
+import { templateDir } from "./converter-data";
 
 function serverDirs(convertedAppDir) {
-  let javaServerDir = join(convertedAppDir, 'scripts');
-  const serverDir = join(convertedAppDir, 'server');
+  let javaServerDir = join(convertedAppDir, "scripts");
+  const serverDir = join(convertedAppDir, "server");
   let nodeServerDir = serverDir;
 
-  if (existsSync(join(serverDir, 'node'))) {
-    nodeServerDir = join(serverDir, 'node');
+  if (existsSync(join(serverDir, "node"))) {
+    nodeServerDir = join(serverDir, "node");
   }
 
-  if (existsSync(join(serverDir, 'java'))) {
-    javaServerDir = join(serverDir, 'java');
+  if (existsSync(join(serverDir, "java"))) {
+    javaServerDir = join(serverDir, "java");
   }
 
   return {
     javaServerDir,
-    nodeServerDir,
+    nodeServerDir
   };
 }
 
@@ -27,42 +34,56 @@ function setUpApplicationFiles(
   applicationName,
   convertedAppDir,
   conversionMetadata,
-  defaultAspectDir,
+  defaultAspectDir
 ) {
-  copySync(join('..', 'conversion-data', applicationName), join(convertedAppDir));
-  copySync(join(templateDir, '.babelrc'), join(convertedAppDir, '.babelrc'));
   copySync(
-    join(defaultAspectDir, 'unbundled-resources'),
-    join(convertedAppDir, 'public/dev/unbundled-resources'),
+    join("..", "conversion-data", applicationName),
+    join(convertedAppDir)
   );
-  copySync(join(defaultAspectDir, 'unbundled-resources'), join(convertedAppDir, 'public/dev'));
+  copySync(join(templateDir, ".babelrc"), join(convertedAppDir, ".babelrc"));
+  copySync(
+    join(defaultAspectDir, "unbundled-resources"),
+    join(convertedAppDir, "public/dev/unbundled-resources")
+  );
+  copySync(
+    join(defaultAspectDir, "unbundled-resources"),
+    join(convertedAppDir, "public/dev")
+  );
 
   const {
     javaServerDir,
-    nodeServerDir,
+    nodeServerDir
   } = serverDirs(convertedAppDir);
 
-  copySync(conversionMetadata.privateKeyFileLocation, join(nodeServerDir, 'privatekey.pem'));
-  copySync(join(conversionMetadata.brjsApplicationDir, 'WEB-INF'), join(javaServerDir, 'WEB-INF'));
+  copySync(
+    conversionMetadata.privateKeyFileLocation,
+    join(nodeServerDir, "privatekey.pem")
+  );
+  copySync(
+    join(conversionMetadata.brjsApplicationDir, "WEB-INF"),
+    join(javaServerDir, "WEB-INF")
+  );
 }
 
 // Given an application populate its `package.json` with all the newly created packages as dependencies.
 function populateApplicationPackageJSON(
   applicationName,
   convertedAppDir,
-  { packagesDir, packagesDirName, packagesThatShouldBeLibs },
+  { packagesDir, packagesDirName, packagesThatShouldBeLibs }
 ) {
   const appPackageJSON = readJsonSync(
-    join('..', 'conversion-data', applicationName, 'package.json'),
+    join("..", "conversion-data", applicationName, "package.json")
   );
-  const appPackageJSONFileLocation = join(convertedAppDir, 'package.json');
+  const appPackageJSONFileLocation = join(convertedAppDir, "package.json");
 
   for (const packageDir of readdirSync(packagesDir)) {
     const isNotLib = packagesThatShouldBeLibs.includes(packageDir) === false;
     const isDirectory = statSync(join(packagesDir, packageDir)).isDirectory();
 
     if (isNotLib && isDirectory) {
-      appPackageJSON.dependencies[packageDir] = `file:../../${packagesDirName}/${packageDir}`;
+      appPackageJSON.dependencies[
+        packageDir
+      ] = `file:../../${packagesDirName}/${packageDir}`;
     }
   }
 
@@ -74,32 +95,39 @@ function createApplication(
   applicationName,
   conversionMetadata,
   defaultAspectDir,
-  conversionDataApplicationName,
+  conversionDataApplicationName
 ) {
-  const convertedAppDir = join('apps', applicationName);
+  const convertedAppDir = join("apps", applicationName);
 
   setUpApplicationFiles(
     conversionDataApplicationName || applicationName,
     convertedAppDir,
     conversionMetadata,
-    defaultAspectDir,
+    defaultAspectDir
   );
   populateApplicationPackageJSON(
     conversionDataApplicationName || applicationName,
     convertedAppDir,
-    conversionMetadata,
+    conversionMetadata
   );
 }
 
 // Create application and application variants directories.
 export function createApplicationAndVariants(conversionMetadata) {
-  const { applicationName, conversionDataApplicationName, packagesDir } = conversionMetadata;
-  const defaultAspectDir = join(packagesDir, `${applicationName}-default-aspect`);
+  const {
+    applicationName,
+    conversionDataApplicationName,
+    packagesDir
+  } = conversionMetadata;
+  const defaultAspectDir = join(
+    packagesDir,
+    `${applicationName}-default-aspect`
+  );
 
   createApplication(
     applicationName,
     conversionMetadata,
     defaultAspectDir,
-    conversionDataApplicationName,
+    conversionDataApplicationName
   );
 }
