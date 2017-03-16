8676e5b66fa4bf51216817b4deefdabcf6d9d270
Brian Di Palma <offler@gmail.com>
2017-03-16 14:18:14 +0000

    refactor(brjs-converter): Format with prettier

--- brjs-app-converter/src/convert-app.js
+++ brjs-app-converter/src/convert-app.js
@@ -1,29 +1,22 @@
-import {
-	readdirSync
-} from 'fs-extra';
+import { readdirSync } from 'fs-extra';
 
-import {
-	moveAspectCode
-} from './convert-aspect';
-import {
-	moveAndConvertBladesCode,
-	moveBladesetCode,
-} from './convert-bladeset';
+import { moveAspectCode } from './convert-aspect';
+import { moveAndConvertBladesCode, moveBladesetCode } from './convert-bladeset';
 
 // Move all code in bladesets/blades/aspects into the packages directory.
 export function moveBRJSApplicationCodeToPackages(conversionMetadata) {
-	const brjsApplicationDir = conversionMetadata.brjsApplicationDir;
-	const moveBRJSAppCodePromises = readdirSync(brjsApplicationDir).map((fileName) => {
-		if (/^.*-bladeset$/.test(fileName)) {
-			return moveBladesetCode(conversionMetadata, fileName);
-		} else if (/^.*-aspect$/.test(fileName)) {
-			moveAspectCode(conversionMetadata, fileName);
-		} else if (fileName === 'blades') {
-			moveAndConvertBladesCode(brjsApplicationDir, '', conversionMetadata);
-		}
+  const brjsApplicationDir = conversionMetadata.brjsApplicationDir;
+  const moveBRJSAppCodePromises = readdirSync(brjsApplicationDir).map(fileName => {
+    if (/^.*-bladeset$/.test(fileName)) {
+      return moveBladesetCode(conversionMetadata, fileName);
+    } else if (/^.*-aspect$/.test(fileName)) {
+      moveAspectCode(conversionMetadata, fileName);
+    } else if (fileName === 'blades') {
+      moveAndConvertBladesCode(brjsApplicationDir, '', conversionMetadata);
+    }
 
-		return Promise.resolve();
-	});
+    return Promise.resolve();
+  });
 
-	return Promise.all(moveBRJSAppCodePromises);
+  return Promise.all(moveBRJSAppCodePromises);
 }
--- brjs-app-converter/src/post-conversion-script.js
+++ brjs-app-converter/src/post-conversion-script.js
@@ -1,43 +1,31 @@
-import {
-	sep
-} from 'path';
+import { sep } from 'path';
 
-import {
-	copySync
-} from 'fs-extra';
+import { copySync } from 'fs-extra';
 import glob from 'glob';
 
-import {
-	copyPackageFoldersToNewLocations,
-	deleteUnusedFiles
-} from './convert-packages';
+import { copyPackageFoldersToNewLocations, deleteUnusedFiles } from './convert-packages';
 
 export function runPostConversionScript(conversionMetadata, convertPackages) {
-	let postConversionScript = () => {
-		// The default post conversion script does nothing.
-	};
-	const scriptName = `post-conversion-${conversionMetadata.applicationName}`;
-	// Convert Windows separator to Unix style for module URIs.
-	const moduleURIPrefix = process
-		.cwd()
-		.split(sep)
-		.join('/');
-	// The location has to be absolute, cannot fathom why, even when correctly relative to `cwd`.
-	const scriptLocation = `${moduleURIPrefix}/../conversion-data/${scriptName}`;
+  let postConversionScript = () => {
+    // The default post conversion script does nothing.
+  };
+  const scriptName = `post-conversion-${conversionMetadata.applicationName}`;
+  // Convert Windows separator to Unix style for module URIs.
+  const moduleURIPrefix = process.cwd().split(sep).join('/');
+  // The location has to be absolute, cannot fathom why, even when correctly relative to `cwd`.
+  const scriptLocation = `${moduleURIPrefix}/../conversion-data/${scriptName}`;
 
-	try {
-		postConversionScript = require(scriptLocation); // eslint-disable-line
-	} catch (err) {
-		// Ignore error if this application has no post conversion script to run.
-	}
+  try {
+    postConversionScript = require(scriptLocation); // eslint-disable-line
+  } catch (err) {
+    // Ignore error if this application has no post conversion script to run.
+  }
 
-	postConversionScript(
-		conversionMetadata, {
-			convertPackages,
-			copyPackageFoldersToNewLocations,
-			copySync,
-			deleteUnusedFiles,
-			glob
-		}
-	);
+  postConversionScript(conversionMetadata, {
+    convertPackages,
+    copyPackageFoldersToNewLocations,
+    copySync,
+    deleteUnusedFiles,
+    glob,
+  });
 }
