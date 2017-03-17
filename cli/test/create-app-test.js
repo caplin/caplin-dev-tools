07b6cb45a044532c4f075df91faae69f3b246687
Brian Di Palma <offler@gmail.com>
2017-03-17 17:14:26 +0000

    fix(cli): Server code now moved into `node` folder

--- cli/test/create-app-test.js
+++ cli/test/create-app-test.js
@@ -1,4 +1,3 @@
-
 var path = require("path");
 var execFile = require("child_process").execFile;
 var rimraf = require("rimraf");
@@ -30,11 +29,11 @@ describe("create-app", () => {
 
   it(
     "should throw an error if apps and packages-caplin directories are now found",
-    (errorThrown) => {
+    errorThrown => {
       var cp = execFile("node", ["./index.js", "create-app", "newapp"]);
       var stdOutput = "", done = false;
 
-      cp.stdout.on("data", (data) => {
+      cp.stdout.on("data", data => {
         stdOutput += data;
 
         if (stdOutput.indexOf("no such file or directory") !== -1 && !done) {
@@ -47,13 +46,13 @@ describe("create-app", () => {
 
   it(
     "should generate an app folder if apps and packages-caplin directories are found",
-    (allFilesCreated) => {
+    allFilesCreated => {
       initDirectories();
 
       var cp = execFile("node", ["./index.js", "create-app", "newapp"]);
       var stdOutput;
 
-      cp.stdout.on("data", (data) => {
+      cp.stdout.on("data", data => {
         stdOutput += data;
 
         if (stdOutput.indexOf("Created") > -1) {
@@ -61,8 +60,7 @@ describe("create-app", () => {
           assert.file("apps/newapp/src/index.js");
           assert.file("apps/newapp/package.json");
           assert.file("apps/newapp/webpack.config.js");
-          assert.file("apps/newapp/server/server.js");
-          assert.file("apps/newapp/server/webpack.js");
+          assert.file("apps/newapp/server/node/server.js");
 
           if (
             stdOutput.indexOf("Now cd into apps/newapp and run npm install") >
@@ -76,13 +74,13 @@ describe("create-app", () => {
     }
   );
 
-  it("should prompt for an app name", (prompted) => {
+  it("should prompt for an app name", prompted => {
     initDirectories();
 
     var cp = execFile("node", ["./index.js", "create-app"]);
     var expected = "What do you want to name your app:";
 
-    cp.stdout.on("data", (data) => {
+    cp.stdout.on("data", data => {
       if (data.indexOf(expected) > -1) {
         prompted();
       }
