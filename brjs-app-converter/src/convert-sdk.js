3269a224ee5ac8f13b14822e1907655c2960d8bc
Brian Di Palma <offler@gmail.com>
2017-03-20 12:58:13 +0000

    refactor(brjs-converter): Format with prettier

--- brjs-app-converter/src/convert-sdk.js
+++ brjs-app-converter/src/convert-sdk.js
@@ -1,35 +1,32 @@
-import {
-	join
-} from 'path';
+import { join } from "path";
 
-import {
-	copySync,
-	readdirSync
-} from 'fs-extra';
+import { copySync, readdirSync } from "fs-extra";
 
-import {
-	convertLib
-} from './convert-libs';
+import { convertLib } from "./convert-libs";
 
 // Copy all SDK libs to packages and convert them to npm packages.
-export function convertSDKToPackages({packagesDir, sdkJSLibrariesDir}) {
-	const packagesContentsFileNames = readdirSync(packagesDir);
-	const sdkConvertedPromises = readdirSync(sdkJSLibrariesDir)
-		.map((packageName) => {
-			// We don't want to copy over packages that have name clashes because those packages came from the
-			// application and BRJS would chose them over the SDK ones when bundling.
-			if (packagesContentsFileNames.includes(packageName)) {
-				console.log(`Skipping conversion of '${packageName}' as the package already exists`); //eslint-disable-line
-			} else {
-				const packageDir = join(packagesDir, packageName);
+export function convertSDKToPackages({ packagesDir, sdkJSLibrariesDir }) {
+  const packagesContentsFileNames = readdirSync(packagesDir);
+  const sdkConvertedPromises = readdirSync(
+    sdkJSLibrariesDir
+  ).map(packageName => {
+    // We don't want to copy over packages that have name clashes because those
+    // packages came from the application and BRJS would chose them over the SDK
+    // ones when bundling.
+    if (packagesContentsFileNames.includes(packageName)) {
+      console.log(
+        `Skipping conversion of '${packageName}' as the package already exists`
+      ); //eslint-disable-line
+    } else {
+      const packageDir = join(packagesDir, packageName);
 
-				copySync(join(sdkJSLibrariesDir, packageName), packageDir);
+      copySync(join(sdkJSLibrariesDir, packageName), packageDir);
 
-				return convertLib(packageDir, packageName);
-			}
+      return convertLib(packageDir, packageName);
+    }
 
-			return Promise.resolve();
-		});
+    return Promise.resolve();
+  });
 
-	return Promise.all(sdkConvertedPromises);
+  return Promise.all(sdkConvertedPromises);
 }
