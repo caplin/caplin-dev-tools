026661ef6582cbe37e0b91fb9c1d9689dc0967f3
Natalia Markaki <nataliam@caplin.com>
2017-03-27 15:27:07 +0100

    CTNXT-646: Refactor create react component command

--- cli/templates/component-react/_test-ut/react-component-test.js
+++ cli/templates/component-react/_test-ut/react-component-test.js
@@ -1,11 +1,12 @@
 import chai from 'chai';
-import { mount, shallow } from 'enzyme';
-import chaiEnzyme from 'chai-enzyme'
 import React from 'react';
+import {
+			renderIntoDocument,
+			findRenderedDOMComponentWithClass,
+			findRenderedDOMComponentWithTag,
+			Simulate
+		} from 'react-addons-test-utils';
 import {{componentName}} from '../react-component';
-
-const expect = chai.expect;
-chai.use(chaiEnzyme());
 
 describe('Testing {{componentName}} Initial', () => {
 	let componentWrapper = null;
@@ -16,18 +17,31 @@ describe('Testing {{componentName}} Initial', () => {
 
 	it('should have count of zero and assigned owner when first loaded', () => {
 		//given
-		componentWrapper = shallow(<{{componentName}} owner="Bob"/>);
+		componentWrapper = renderIntoDocument(
+		   <{{componentName}} owner="Bob"/>
+		);
+
 		//then
-		expect(componentWrapper.find('.counter')).to.have.text('count:0');
-		expect(componentWrapper.find('.heading')).to.have.text('This {{componentName}} Component belongs to Bob');
+		const counter = findRenderedDOMComponentWithClass(componentWrapper, 'counter');
+		const counterText = counter.textContent;
+		chai.assert.equal(counterText, 'count:0');
+
+		const heading = findRenderedDOMComponentWithClass(componentWrapper, 'heading');
+		const headingText = heading.textContent;
+		chai.assert.equal(headingText, 'This {{componentName}} Component belongs to Bob');
 	})
 
 	it('should increment count when button is clicked', () => {
 		//given
-		componentWrapper = shallow(<{{componentName}} />);
+		componentWrapper = renderIntoDocument(
+		   <{{componentName}} />
+		);
 		//when
-		componentWrapper.find('button').simulate('click');
+		const counterButton = findRenderedDOMComponentWithTag(componentWrapper, 'button');
+		Simulate.click(counterButton);
 		//then
-		expect(componentWrapper.find('.counter')).to.have.text('count:1');
+		const counter = findRenderedDOMComponentWithClass(componentWrapper, 'counter');
+		const counterText = counter.textContent;
+		chai.assert.equal(counterText, 'count:1');
 	})
 });
