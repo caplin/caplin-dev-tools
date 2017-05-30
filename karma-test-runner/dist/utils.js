8f8e7d297d018b8ddd6309b218179b8e3eb05564
jngo <jason.ngo@caplin.com>
2017-05-30 14:25:55 +0100

    (fix) : capture errors separate from failures when running tests

--- karma-test-runner/dist/utils.js
+++ karma-test-runner/dist/utils.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const { Server } = require("karma");
+const { getTotalTime } = require("karma-caplin-dots-reporter");
+const { getTotalTestsSkipped } = require("karma-caplin-dots-reporter");
 
 function runOnlyATs(args) {
   return args.ats || args.ATs || args._.includes("--ats") || args._.includes("--ATs") || args._.includes("ats") || args._.includes("ATs") || false;
@@ -47,20 +49,38 @@ function getTestBrowser(commandLineArgs) {
 
 module.exports.getTestBrowser = getTestBrowser;
 
-function showSummary({ success, failed, error, errors }, devMode) {
-  if (devMode === false) {
-    console.log("\n== Test Report ==");
+function showSummary({ success, failed, error, errors, failures }, devMode) {
+  if (!devMode) {
+    console.log("\n====== Test Report ======");
 
     if (failed > 0 || error) {
       console.log("\n\x1b[41m\x1b[30mTesting ended with failures/errors!\x1b[0m");
-      console.log(`${errors.map(({ packageName, error }) => `\nTest failed in: \x1b[35m${packageName}\n${error}`).join("\n")}\n`);
-    } else {
+
+      if (errors.length > 0) {
+        console.log(`${errors.map(({ packageName, error }) => `\nTest errored in: \x1b[35m${packageName}\n${error}`).join("\n")}\n`);
+      }
+
+      if (failures.length > 0) {
+        console.log(`${failures.map(({ packageName, failure }) => `\nTest failed in: \x1b[35m${packageName}\n${failure}`).join("\n")}\n`);
+      }
+    }
+
+    if (getTotalTime() === 0) {
+      console.log("\n\x1b[41m\x1b[30mNo tests were ran, please check your package name is correct.\x1b[0m");
+      process.exit(1);
+    }
+
+    if (failures.length === 0 && errors.length === 0) {
       console.log("\n\x1b[42m\x1b[30mTesting ended with no failures!\x1b[0m");
     }
 
-    console.log(`\x1b[35mPassed:\x1b[0m ${success}`);
-    console.log(`\x1b[35mFailed:\x1b[0m ${failed}`);
-    console.log(`\x1b[35mErrors:\x1b[0m ${error ? "Yes" : "No"}`);
+    if (!error) {
+      console.log(`\x1b[35mPassed:\x1b[0m ${success}`);
+      console.log(`\x1b[35mFailed:\x1b[0m ${failures.length}`);
+      console.log(`\x1b[35mErrors:\x1b[0m ${errors.length}`);
+      console.log(`\x1b[35mTotal Tests Skipped:\x1b[0m ${getTotalTestsSkipped()}`);
+      console.log(`\x1b[35mTotal Time:\x1b[0m ${getTotalTime() / 1000 + ' secs'}`);
+    }
 
     if (failed > 0 || error) {
       process.exit(1);
