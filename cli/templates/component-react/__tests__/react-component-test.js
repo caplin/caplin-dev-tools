a23f5b11348a136c8a8974a1ee85a37ed4f5c8b9
John Wickers <john.wickers@caplin.com>
2017-06-12 15:35:51 +0100

    fix jest test structure, use generic react component

--- cli/templates/component-react/__tests__/react-component-test.js
+++ cli/templates/component-react/__tests__/react-component-test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 
-import Globe from '../react-component';
+import {{componentName}} from '../react-component';
 
 describe('example test', function() {
-  it('should have count of zero when first loaded', () => {
-    const wrapper = shallow(<Globe owner="Bob" />);
+  test('should have count of zero when first loaded', () => {
+    const wrapper = shallow(<{{componentName}} owner="Bob" />);
     const counterText = wrapper.find('.counter').first().text();
     expect(counterText).toEqual('count:0');
   });
