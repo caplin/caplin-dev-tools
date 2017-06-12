1502c4046f73eb0fe48c1a8ef6b9a56e2baaf5c9
John Wickers <john.wickers@caplin.com>
2017-06-12 14:45:31 +0100

    modify react component template to use enzyme/jest

--- cli/templates/component-react/__tests__/react-component-test.js
+++ cli/templates/component-react/__tests__/react-component-test.js
@@ -1,47 +1,12 @@
-import chai from 'chai';
 import React from 'react';
-import {
-	renderIntoDocument,
-	findRenderedDOMComponentWithClass,
-	findRenderedDOMComponentWithTag,
-	Simulate
-} from 'react-addons-test-utils';
-import {{componentName}} from '../react-component';
+import { shallow, mount, render } from 'enzyme';
 
-describe('Testing {{componentName}} Initial', () => {
-	let componentWrapper = null;
+import Globe from '../react-component';
 
-	afterEach(() => {
-		componentWrapper = null;
-	});
-
-	it('should have count of zero and assigned owner when first loaded', () => {
-		//given
-		componentWrapper = renderIntoDocument(
-		   <{{componentName}} owner="Bob"/>
-		);
-
-		//then
-		const counter = findRenderedDOMComponentWithClass(componentWrapper, 'counter');
-		const counterText = counter.textContent;
-		chai.assert.equal(counterText, 'count:0');
-
-		const heading = findRenderedDOMComponentWithClass(componentWrapper, 'heading');
-		const headingText = heading.textContent;
-		chai.assert.equal(headingText, 'This {{componentName}} Component belongs to Bob');
-	})
-
-	it('should increment count when button is clicked', () => {
-		//given
-		componentWrapper = renderIntoDocument(
-		   <{{componentName}} />
-		);
-		//when
-		const counterButton = findRenderedDOMComponentWithTag(componentWrapper, 'button');
-		Simulate.click(counterButton);
-		//then
-		const counter = findRenderedDOMComponentWithClass(componentWrapper, 'counter');
-		const counterText = counter.textContent;
-		chai.assert.equal(counterText, 'count:1');
-	})
+describe('example test', function() {
+  it('should have count of zero when first loaded', () => {
+    const wrapper = shallow(<Globe owner="Bob" />);
+    const counterText = wrapper.find('.counter').first().text();
+    expect(counterText).toEqual('count:0');
+  });
 });
