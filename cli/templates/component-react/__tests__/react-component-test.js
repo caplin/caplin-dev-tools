049b8e27866fdd71221a6d962f655a73ddb15ad6
John Wickers <john.wickers@caplin.com>
2017-06-13 10:16:15 +0100

    add tests for simulate click and props

--- cli/templates/component-react/__tests__/react-component-test.js
+++ cli/templates/component-react/__tests__/react-component-test.js
@@ -3,10 +3,21 @@ import { shallow, mount, render } from 'enzyme';
 
 import {{componentName}} from '../react-component';
 
-describe('example test', function() {
-  test('should have count of zero when first loaded', () => {
+describe('example tests', function() {
+  test('should have correct header when passed owner with props', () => {
     const wrapper = shallow(<{{componentName}} owner="Bob" />);
+    const headerText = wrapper.find('.heading').first().text();
+    expect(headerText).toEqual('This {{componentName}} Component belongs to Bob');
+  });
+  test('should have count of zero when first loaded', () => {
+    const wrapper = shallow(<{{componentName}} />);
     const counterText = wrapper.find('.counter').first().text();
     expect(counterText).toEqual('count:0');
+  });
+  test('should update counter when button pressed', () => {
+    const wrapper = shallow(<{{componentName}} />);
+    wrapper.find('button').first().simulate('click');
+    const counterText = wrapper.find('.counter').first().text();
+    expect(counterText).toEqual('count:1');
   });
 });
