0987c7011d460be1320aeb1e51ef0d545c19ec14
John Wickers <john.wickers@caplin.com>
2017-06-14 14:27:13 +0100

    prettier formatting

--- cli/templates/component-react/__tests__/react-component-test.js
+++ cli/templates/component-react/__tests__/react-component-test.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import React from "react";
+import { shallow, mount, render } from "enzyme";
 
-import {{componentName}} from '../react-component';
+import {{componentName}} from "../react-component";
 
-describe('example tests', function() {
-  test('should have correct header when passed owner with props', () => {
+describe("example tests", function() {
+  test("should have correct header when passed owner with props", () => {
     const wrapper = shallow(<{{componentName}} owner="Bob" />);
-    const headerText = wrapper.find('.heading').first().text();
-    expect(headerText).toEqual('This {{componentName}} Component belongs to Bob');
+    const headerText = wrapper.find(".heading").first().text();
+    expect(headerText).toEqual("This {{componentName}} Component belongs to Bob");
   });
-  test('should have count of zero when first loaded', () => {
+  test("should have count of zero when first loaded", () => {
     const wrapper = shallow(<{{componentName}} />);
-    const counterText = wrapper.find('.counter').first().text();
-    expect(counterText).toEqual('count:0');
+    const counterText = wrapper.find(".counter").first().text();
+    expect(counterText).toEqual("count:0");
   });
-  test('should update counter when button pressed', () => {
+  test("should update counter when button pressed", () => {
     const wrapper = shallow(<{{componentName}} />);
-    wrapper.find('button').first().simulate('click');
-    const counterText = wrapper.find('.counter').first().text();
-    expect(counterText).toEqual('count:1');
+    wrapper.find("button").first().simulate("click");
+    const counterText = wrapper.find(".counter").first().text();
+    expect(counterText).toEqual("count:1");
   });
 });
