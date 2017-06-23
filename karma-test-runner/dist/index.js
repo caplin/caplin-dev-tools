8c440cb84f738e05a6b429eb0d6314bd0af707b9
Brian Di Palma <offler@gmail.com>
2017-06-23 15:38:41 +0100

    fix(karma-runner): Speed up webpack compile by removing unused entry

--- karma-test-runner/dist/index.js
+++ karma-test-runner/dist/index.js
@@ -76,7 +76,8 @@ function createPackageKarmaConfig({ files = [], frameworks = [], packageDirector
   const karmaFiles = [...files, testEntry];
   const plugins = [...webpackConfig.plugins, new DefinePlugin({ PACKAGE_DIRECTORY: `"${packageDirectory}"` })];
   const packageWebpackConfig = Object.assign({}, webpackConfig, {
-    entry: testEntry,
+    // No `entry` as `karma-webpack` doesn't need it, creates second chunk.
+    entry: undefined,
     plugins
   });
   const testsType = testEntry === utsTestEntry ? "UTs" : "ATs";
