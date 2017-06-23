232c70a5f79193787ca73a53b3ebd2a28201df6c
stevesouth <stephen.south@caplin.com>
2017-06-23 10:06:29 +0100

    Add a test command to the component template

--- cli/templates/component-react/__tests__/react-component-test.js
+++ cli/templates/component-react/__tests__/react-component-test.js
@@ -6,18 +6,18 @@ import {{componentName}} from "../react-component";
 describe("example tests", function() {
   test("should have correct header when passed owner with props", () => {
     const wrapper = shallow(<{{componentName}} owner="Bob" />);
-    const headerText = wrapper.find(".heading").first().text();
+    const headerText = wrapper.find(".{{componentName}}-heading").first().text();
     expect(headerText).toEqual("This {{componentName}} Component belongs to Bob");
   });
   test("should have count of zero when first loaded", () => {
     const wrapper = shallow(<{{componentName}} />);
-    const counterText = wrapper.find(".counter").first().text();
+    const counterText = wrapper.find(".{{componentName}}-counter").first().text();
     expect(counterText).toEqual("count:0");
   });
   test("should update counter when button pressed", () => {
     const wrapper = shallow(<{{componentName}} />);
     wrapper.find("button").first().simulate("click");
-    const counterText = wrapper.find(".counter").first().text();
+    const counterText = wrapper.find(".{{componentName}}-counter").first().text();
     expect(counterText).toEqual("count:1");
   });
 });
