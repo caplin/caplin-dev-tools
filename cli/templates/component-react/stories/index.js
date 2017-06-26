4180103e46e154d93a873d9a834c6e1cf588ba25
NataliaMarkaki <nataliam@caplin.com>
2017-06-26 10:37:50 +0100

    CT5UP-196: Place storybook on app level

--- cli/templates/component-react/stories/index.js
+++ cli/templates/component-react/stories/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Component from '../react-component';
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Tweet', module)
+storiesOf(`{{componentName}}`, module)
   .add('with steve', () => <Component owner='steve' />)
   .add('with natalia', () => <Component owner='natalia' />)
