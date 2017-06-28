d4e8f42b4bea6d6b2a04eda325f998eead09aa2c
danielo <daniel.owen@caplin.com>
2017-06-28 08:49:37 +0100

    CT5UP-112 initial boilerplate commit

---
+++ selenium-boilerplate/src/test/java/caplin/example/selectors/BaseSelectors.java
@@ -0,0 +1,8 @@
+package caplin.example.selectors;
+
+public class BaseSelectors {
+
+    public static String HEADER_BAR_LOGIN_BUTTON = "#pt-login";
+
+    public static String HEADER_BAR_LOGEDIN_ICON = "#pt-userpage";
+}
---
+++ selenium-boilerplate/src/test/java/caplin/example/selectors/LoginSelectors.java
@@ -0,0 +1,8 @@
+package caplin.example.selectors;
+
+public class LoginSelectors {
+    public static String USERNAME_TEXTBOX = "#wpName1";
+    public static String PASSWORD_BOX = "#wpPassword1";
+
+    public static String LOGIN_BUTTON = "#wpLoginAttempt";
+}
