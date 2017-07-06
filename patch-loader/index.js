5cb60c582a373a16d6004b583d1e3f37f2cdf241
Brian Di Palma <offler@gmail.com>
2017-07-06 12:39:51 +0100

    feat(HardSourcePlugin): Add support to webpack config and patch loader

--- patch-loader/index.js
+++ patch-loader/index.js
@@ -1,7 +1,17 @@
 const loaderUtils = require("loader-utils");
 
+function findPatchLoader({ loader }) {
+  return loader === "@caplin/patch-loader";
+}
+
 module.exports = function patchLoader(moduleSource) {
-  const options = loaderUtils.getOptions(this);
+  let options = loaderUtils.getOptions(this);
+
+  // This check is required as a work around for this issue
+  // https://github.com/mzgoddard/hard-source-webpack-plugin/issues/149
+  if (options.appendModulePatch === undefined) {
+    options = this.options.module.rules.find(findPatchLoader).options;
+  }
 
   return options.appendModulePatch(this, moduleSource);
 };
