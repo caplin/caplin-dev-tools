b2d93fbdffd073b07a67a2186c3d46980ca910c5
danielo <daniel.owen@caplin.com>
2017-07-10 15:17:53 +0100

    CT5UP-212 minor change to parameter name

--- cli/utils/copyTemplate.js
+++ cli/utils/copyTemplate.js
@@ -3,7 +3,7 @@ const path = require("path");
 const chalk = require("chalk");
 const fs = require("fs");
 
-module.exports = function(template, target, consts) {
+module.exports = function(template, target, vars) {
   return new Promise(function(resolve, reject) {
     var inDir = path.join(__dirname, "..", "templates", template);
 
@@ -17,7 +17,7 @@ module.exports = function(template, target, consts) {
           if (filePath.indexOf("react-component") !== -1) {
             const newFileName = filePath.replace(
               /react-component/g,
-              `${consts.componentName}`
+              `${vars.componentName}`
             );
             fs.rename(filePath, newFileName, err => {
               if (err) {
