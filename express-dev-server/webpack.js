5cb51e1dd96c58c514ab5b4d1cbcba8044e06aca
NataliaMarkaki <natalia.markaki@caplin.com>
2017-07-31 15:10:51 +0100

    CT5UP-252: Change the way webpack errors and warnings are displayed

--- express-dev-server/webpack.js
+++ express-dev-server/webpack.js
@@ -2,23 +2,44 @@ const webpack = require("webpack");
 const webpackDevMiddleware = require("webpack-dev-middleware");
 const webpackHotMiddleware = require("webpack-hot-middleware");
 const parseArgs = require("minimist");
+const formatWebpackMessages = require("react-dev-utils/formatWebpackMessages");
 
 const { hot } = parseArgs(process.argv.slice(2));
+let isFirstTime = true;
 
 module.exports = (application, webpackConfig) => {
   const compiler = webpack(webpackConfig);
+
+  compiler.plugin("done", stats => {
+    const messages = formatWebpackMessages(stats.toJson({}, true));
+    const isSuccessful = !messages.errors.length && !messages.warnings.length;
+
+    if (isSuccessful) {
+      if (!isFirstTime) {
+        console.log("Compiled successfully!");
+      }
+      isFirstTime = false;
+    }
+
+    if (messages.errors.length) {
+      console.log("Failed to compile.\n");
+      console.log(messages.errors.join("\n\n"));
+      return;
+    }
+
+    if (messages.warnings.length) {
+      console.log("Compiled with warnings.\n");
+      console.log(messages.warnings.join("\n\n"));
+    }
+  });
+
   const devMiddlewareOptions = {
     // Required, path to bind the middleware to.
     publicPath: webpackConfig.output.publicPath,
-    noInfo: false,
-    quiet: false,
-    stats: {
-      assets: false,
-      colors: true,
-      chunks: false,
-      // Turn output from the extract text plugin off.
-      children: false
-    }
+    clientLogLevel: "none",
+    noInfo: true,
+    quiet: true,
+    stats: "none"
   };
 
   if (hot) {
@@ -27,7 +48,7 @@ module.exports = (application, webpackConfig) => {
 
     application.use(
       webpackHotMiddleware(compiler, {
-        log: console.log,
+        log: () => {},
         path: "/__webpack_hmr",
         heartbeat: 10 * 1000
       })
