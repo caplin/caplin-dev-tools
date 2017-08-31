21a3900c523cfc701ec1388d7b5709ba1bf8c56f
NataliaMarkaki <nataliam@caplin.com>
2017-08-31 13:19:12 +0100

    CT5UP-306: Check npm version and show warning for v.5

--- cli/commands/create-app.js
+++ cli/commands/create-app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const chalk = require("chalk");
 const fs = require("fs");
 const copyTemplate = require("../utils/copyTemplate");
+const globalModules = require("global-modules");
 
 function workingDirectoryErrorMessage(dir) {
   return `'${dir}' directory not found.
@@ -65,6 +66,21 @@ module.exports = {
           Now cd into apps/${name} and run ${chalk.blue("npm install")}
           `
       );
+
+      const npmVersion = fs
+        .readdirSync(
+          path.join(globalModules, "..", "..", "npm-cache", "npmlog")
+        )
+        .pop();
+
+      if (npmVersion.startsWith("5")) {
+        console.log(
+          `You may have problems using some of the tooling with your current version of npm. Whilst npm address these issues we recommend using npm 4`
+        );
+        console.log(
+          `To change your npm version run ${chalk.blue("npm i -g npm@4")}`
+        );
+      }
     });
   }
 };
