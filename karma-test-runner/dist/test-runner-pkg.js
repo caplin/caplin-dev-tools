0678ea45b8f0bbd73f09a3651487c53d6d87f06d
Brian Di Palma <offler@gmail.com>
2017-09-05 16:38:17 +0100

    fix(karma-runner): Ignore tests inside a package's `node_modules`

--- karma-test-runner/dist/test-runner-pkg.js
+++ karma-test-runner/dist/test-runner-pkg.js
@@ -3,13 +3,17 @@
 const { sync } = require("glob");
 
 function doesPkgHaveATs(packageDirectory) {
-  return sync(`${packageDirectory}/**/_test-at/**/*.js`).length > 0;
+  const globOpts = { ignore: [`${packageDirectory}/node_modules/**`] };
+
+  return sync(`${packageDirectory}/**/_test-at/**/*.js`, globOpts).length > 0;
 }
 
 module.exports.doesPkgHaveATs = doesPkgHaveATs;
 
 function doesPkgHaveUTs(packageDirectory) {
-  return sync(`${packageDirectory}/**/_test-ut/**/*.js`).length > 0;
+  const globOpts = { ignore: [`${packageDirectory}/node_modules/**`] };
+
+  return sync(`${packageDirectory}/**/_test-ut/**/*.js`, globOpts).length > 0;
 }
 
 module.exports.doesPkgHaveUTs = doesPkgHaveUTs;
