5105e5757f63fd326d0cc0429d86bb3e5582191b
Brian Di Palma <offler@gmail.com>
2018-03-09 14:54:32 +0000

    fix(jndi): Support empty string environment values
    e.g. `CAPLIN.LOGOUT.REDIRECT=` where the value is meant
    to be an empty string.

--- express-dev-server/jndi.js
+++ express-dev-server/jndi.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 function jndiTokenReplacer(match, jndiToken) {
-  if (process.env[jndiToken]) {
+  if (process.env.hasOwnProperty(jndiToken)) {
     return process.env[jndiToken];
   }
 
