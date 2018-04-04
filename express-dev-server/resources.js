300e99b5e63cafecd11fcbc40cfc58ebd3d3e9ce
Brian Di Palma <offler@gmail.com>
2018-04-04 12:05:30 +0100

    refactor(unbundled-resources): Clarify purpose of resources module
    Improve comments and variable names.

--- express-dev-server/resources.js
+++ express-dev-server/resources.js
@@ -1,13 +1,14 @@
 const { join } = require("path");
 
-module.exports = (applicationPath, application, appRoot) => {
-  const applicationAspectPrefix = `src${applicationPath}-`;
+module.exports = (applicationName, application, appRoot) => {
+  const defaultAspectPath = `src/${applicationName}-default-aspect`;
+  const unbundledResourcesPath = `${defaultAspectPath}/unbundled-resources`;
 
-  // Serve up unbundled-resources from `node_modules`.
+  // Serve unbundled-resources requests from the default-aspect directory.
   application.get("/unbundled-resources/*", ({ params }, res) => {
-    const resourcePath = `${applicationAspectPrefix}default-aspect/unbundled-resources/${params["0"]}`;
-    const absResourcePath = join(appRoot, resourcePath);
+    const resourcePath = `${unbundledResourcesPath}/${params["0"]}`;
+    const absoluteResourcePath = join(appRoot, resourcePath);
 
-    res.sendFile(absResourcePath);
+    res.sendFile(absoluteResourcePath);
   });
 };
