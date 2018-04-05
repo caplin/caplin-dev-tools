275a0933a09ff69a78b410f8a61942e87c883575
Brian Di Palma <offler@gmail.com>
2018-04-05 11:48:30 +0100

    chore: Update project ESLint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,12 @@
-const baseRules = require("eslint-config-airbnb-base/rules/style");
-
-const [_, ...restricted] = baseRules.rules["no-restricted-syntax"];
-
 module.exports = {
-  extends: ["airbnb", "prettier"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:import/recommended", "prettier"],
   rules: {
-    // The console is the easiest way to provide feedback when executing in node
-    "no-console": 0,
-    // Let's allow for..in https://github.com/airbnb/javascript/issues/1122
-    "no-restricted-syntax": [
-      2,
-      ...restricted.filter(r => r.selector !== "ForOfStatement")
-    ],
-    "no-underscore-dangle": ["error", { allowAfterThis: true }]
+    "import/no-unresolved": [2, { commonjs: true }],
+    "import/unambiguous": 0
   }
 };
