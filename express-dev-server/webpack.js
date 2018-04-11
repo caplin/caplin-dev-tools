5c6bba4936425acde28e0cd53d652da480436ede
Brian Di Palma <offler@gmail.com>
2018-04-11 15:56:16 +0100

    fix(webpack): Log compile success message after first compilation

--- express-dev-server/webpack.js
+++ express-dev-server/webpack.js
@@ -1,41 +1,34 @@
+const logger = require("@caplin/node-logger");
 const webpack = require("webpack");
 const webpackDevMiddleware = require("webpack-dev-middleware");
 const webpackHotMiddleware = require("webpack-hot-middleware");
 const parseArgs = require("minimist");
 const formatWebpackMessages = require("react-dev-utils/formatWebpackMessages");
-const chalk = require("chalk");
+const { yellow } = require("chalk");
 
 const { hot } = parseArgs(process.argv.slice(2));
-let isFirstTime = true;
 
 module.exports = (application, webpackConfig) => {
   const compiler = webpack(webpackConfig);
 
   compiler.plugin("done", stats => {
-    const messages = formatWebpackMessages(stats.toJson({}, true));
+    const jsonStats = stats.toJson({}, true);
+    const messages = formatWebpackMessages(jsonStats);
     const isSuccessful = !messages.errors.length && !messages.warnings.length;
 
     if (isSuccessful) {
-      if (!isFirstTime) {
-        console.log(chalk.yellow("Compiled successfully!"));
-      }
-      isFirstTime = false;
+      logger.info(`Compiled successfully in ${yellow(jsonStats.time)}ms`);
     }
 
     if (messages.errors.length) {
-      console.log(chalk.red("Failed to compile.\n"));
-      console.log(messages.errors.join("\n\n"));
-      return;
-    }
-
-    if (messages.warnings.length) {
-      console.log(chalk.magenta("Compiled with warnings.\n"));
-      console.log(messages.warnings.join("\n\n"));
+      logger.error(`Failed to compile.\n${messages.errors.join("\n\n")}`);
+    } else if (messages.warnings.length) {
+      logger.warn(`Compiled with warnings.\n${messages.warnings.join("\n\n")}`);
     }
   });
 
   // overrides hard-source-log's console output
-  compiler.plugin('hard-source-log', () => {});
+  compiler.plugin("hard-source-log", () => {});
 
   const devMiddlewareOptions = {
     // Required, path to bind the middleware to.
