0c0df817b6d69b3beaa3739604ff12d00d111078
Brian Di Palma <offler@gmail.com>
2018-04-24 12:25:04 +0100

    feat(webpack): Add support for Webpack 4
    BREAKING CHANGE: Drop support for Webpack <4

--- express-dev-server/webpack.js
+++ express-dev-server/webpack.js
@@ -1,48 +1,26 @@
-const logger = require("@caplin/node-logger");
 const webpack = require("webpack");
 const webpackDevMiddleware = require("webpack-dev-middleware");
 const webpackHotMiddleware = require("webpack-hot-middleware");
 const parseArgs = require("minimist");
-const formatWebpackMessages = require("react-dev-utils/formatWebpackMessages");
-const { yellow } = require("chalk");
 
 const { hot } = parseArgs(process.argv.slice(2));
 
 module.exports = (application, webpackConfig) => {
   const compiler = webpack(webpackConfig);
-
-  compiler.plugin("done", stats => {
-    const jsonStats = stats.toJson({}, true);
-    const messages = formatWebpackMessages(jsonStats);
-    const isSuccessful = !messages.errors.length && !messages.warnings.length;
-
-    if (isSuccessful) {
-      logger.info(`Compiled successfully in ${yellow(jsonStats.time)}ms`);
-    }
-
-    if (messages.errors.length) {
-      logger.error(`Failed to compile.\n${messages.errors.join("\n\n")}`);
-    } else if (messages.warnings.length) {
-      logger.warn(`Compiled with warnings.\n${messages.warnings.join("\n\n")}`);
-    }
-  });
-
-  // overrides hard-source-log's console output
-  compiler.plugin("hard-source-log", () => {});
-
   const devMiddlewareOptions = {
     // Required, path to bind the middleware to.
     publicPath: webpackConfig.output.publicPath,
-    clientLogLevel: "none",
-    noInfo: true,
-    quiet: true,
-    stats: "none"
+    stats: {
+      all: false,
+      colors: true,
+      errors: true,
+      moduleTrace: true,
+      timings: true,
+      warnings: true
+    }
   };
 
   if (hot) {
-    devMiddlewareOptions.hot = true;
-    devMiddlewareOptions.historyApiFallback = true;
-
     application.use(
       webpackHotMiddleware(compiler, {
         log: () => {},
