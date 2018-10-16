8802476c25810c5ae73a9d92bdf5d309c23756e5
Brian Di Palma <offler@gmail.com>
2018-10-16 14:46:49 +0100

    feat(proxy-factory): Terminate the server on proxy process errors
    This prevents errors from being missed e.g. when running the development
    stack on a new machine with no `mvn` installed, the Webpack/HTTP server
    would work but the proxy target would fail to start.
    
    While output indicating an error occured is printed out it may be missed
    if there is a lot of other output.
    
    BREAKING CHANGE: It's possible that certain errors don't affect the
    running of the proxy target.

--- express-dev-server/proxy-target-factory.js
+++ express-dev-server/proxy-target-factory.js
@@ -1,9 +1,9 @@
 const { spawn } = require("child_process");
+const logger = require("@caplin/node-logger");
 
 const proxySpawnOptions = {
   // Required for Windows.
-  shell: true,
-  stdio: "inherit"
+  shell: true
 };
 
 module.exports = ({
@@ -15,4 +15,23 @@ module.exports = ({
     "jetty:run",
     "-Djava.util.logging.config.file=server/java/logging.properties"
   ]
-} = {}) => spawn(cmd, args, proxySpawnOptions);
+} = {}) => {
+  const process = spawn(cmd, args, proxySpawnOptions);
+
+  process.stderr.on("data", stderrData => {
+    // Convert from `Buffer` to utf-8 string.
+    const errorMessage = stderrData.toString();
+
+    // Throwing an uncaught error and allowing the process to terminate is safer
+    // than calling process.exit as it won't be truncate error message output.
+    // https://nodejs.org/api/process.html#process_process_exit_code
+    throw new Error(errorMessage);
+  });
+
+  process.stdout.on("data", stdoutData => {
+    // Convert from `Buffer` to utf-8 string.
+    logger.info(stdoutData.toString());
+  });
+
+  return process;
+};
