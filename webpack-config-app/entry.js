57fc1a1d621427ef79c4128d4a5f106f7df3e39b
Alvaro <a.munozsesma@gmail.com>
2018-10-22 14:55:29 +0100

    Changing variant entry point

--- webpack-config-app/entry.js
+++ webpack-config-app/entry.js
@@ -1,12 +1,26 @@
 const { join } = require("path");
+const { existsSync } = require("fs");
 
 module.exports = function configureBundleEntryPoint(
   variant,
   webpackConfig,
   basePath
 ) {
+  let entryFile = "index.js";
+
   // Certain apps can have variant entry points e.g. mobile.
-  const entryFile = variant ? `index-${variant}.js` : "index.js";
+  if (variant) {
+    const customerEntryPoint = join("customers", variant, "index.js");
+    // Keep backwards compatibility for apps that might already be using this
+    const variantEntryPoint = `index-${variant}.js`;
+
+    entryFile = existsSync(customerEntryPoint)
+      ? customerEntryPoint
+      : existsSync(variantEntryPoint)
+        ? variantEntryPoint
+        : entryFile;
+  }
+
   const appEntryPoint = join(basePath, "src", entryFile);
   const reactErrorOverlay = require.resolve("react-error-overlay");
 
