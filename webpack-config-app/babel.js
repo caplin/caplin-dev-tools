e6457dea480d34d42a285fa766b68457cf2830ac
Brian Di Palma <offler@gmail.com>
2018-10-31 11:54:48 +0000

    fix(babel): Filter out node_modules from compilation
    Only compile code in the `src` directory of an app, that was
    installed from `packages-caplin` or from `packages`.

--- webpack-config-app/babel.js
+++ webpack-config-app/babel.js
@@ -2,31 +2,30 @@ const { existsSync, readdirSync } = require("fs");
 const { join, sep } = require("path");
 
 const dirSep = sep === "\\" ? "\\\\" : sep;
-// Find a `node_modules` that is not followed by any further `node_modules`,
-// and extract the path part following it (should be name of imported package).
-const pckNameRE = `node_modules(?!.*node_modules)${dirSep}(.*?)${dirSep}`;
+// Find a `node_modules` that is not followed by any further `node_modules` (in
+// other words the last `node_modules` in a string), and extract the path part
+// following it (should be name of imported package).
+const nodeModulesName = `node_modules(?!.*node_modules)${dirSep}(.*?)${dirSep}`;
+const nodeModulesNameRegExp = new RegExp(nodeModulesName);
+// Find a `packages-caplin` or `packages` and extract the path part following it
+// (should be name of imported local package).
+const pckNameRE = `(?:packages-caplin|packages)${dirSep}(.*?)${dirSep}`;
 const pckNameRegExp = new RegExp(pckNameRE);
 
-function findCaplinPackagesDir(basePath) {
-  // Once mobile and REX are moved into the main `apps` folder this can be
-  // deleted.
-  if (existsSync(join(basePath, "../../packages-caplin"))) {
-    return join(basePath, "../../packages-caplin");
-  }
-
-  return join(basePath, "../../../packages-caplin");
-}
-
-function findAllPackages(basePath) {
-  const caplinPackagesDir = findCaplinPackagesDir(basePath);
-  const clientPackagesDir = join(basePath, "../../packages");
+// Return an array of all the local packages/ones that are installed from the
+// packages-caplin/packages directories.
+function findAllLocalPackages(basePath) {
   const allPackages = [];
+  const caplinPackagesDir = join(basePath, "../../packages-caplin");
+  const clientPackagesDir = join(basePath, "../../packages");
 
   if (existsSync(clientPackagesDir)) {
     allPackages.push(...readdirSync(clientPackagesDir));
   }
 
   if (existsSync(caplinPackagesDir)) {
+    // Filter out the thirdparty packages from the `packages-caplin` list as we
+    // don't want to compile those.
     const caplinPackages = readdirSync(caplinPackagesDir).filter(pckName => {
       const convLib = join(caplinPackagesDir, pckName, "converted_library.js");
 
@@ -39,30 +38,43 @@ function findAllPackages(basePath) {
   return allPackages;
 }
 
+function extractPackageNameFromPath(sourcePath) {
+  const nodeModulesMatch = sourcePath.match(nodeModulesNameRegExp);
+
+  // Code that has been installed inside `node_modules`.
+  if (nodeModulesMatch !== null) {
+    return nodeModulesMatch[1];
+  }
+
+  const packagesMatch = sourcePath.match(pckNameRegExp);
+
+  // Code that has been linked to e.g. "...packages-caplin/ct-grid/GridView.js"
+  if (packagesMatch !== null) {
+    return packagesMatch[1];
+  }
+}
+
 function createIncludeFunction(basePath) {
-  const allPackages = findAllPackages(basePath);
+  const allPackages = findAllLocalPackages(basePath);
+  const isAppSource = new RegExp(`${basePath}${dirSep}src`);
+  const isSourceFile = new RegExp(/\.jsx?$/);
 
   return function includeFunction(sourcePath) {
-    const nodeModulesMatch = sourcePath.match(pckNameRegExp);
+    if (isSourceFile.test(sourcePath)) {
+      if (isAppSource.test(sourcePath)) {
+        return true;
+      }
 
-    if (nodeModulesMatch === null) {
-      // If the file isn't in `node_modules` it's either a source file from
-      // `basePath` (e.g. app's `src` folder) or a file inside a symlinked
-      // development package (using Yarn's `link:` feature`).
-      return true;
+      const packageName = extractPackageNameFromPath(sourcePath);
+
+      return allPackages.includes(packageName);
     }
-
-    const packageName = nodeModulesMatch[1];
-
-    return allPackages.includes(packageName);
   };
 }
 
 module.exports = function configureBabelLoader(webpackConfig, basePath) {
   const babelModulesRule = {
-    test: /\.jsx?$/,
-    // exclude: /(node_modules|bower_components)/,
-    // include: createIncludeFunction(basePath),
+    test: createIncludeFunction(basePath),
     use: {
       loader: "babel-loader",
       options: {
