2121f179efe4054db4f73789098b3e35db635a13
chrisprobably <chris.probably@gmail.com>
2018-11-20 12:10:18 +0000

    More logging to check whats happening with the packages

--- karma-test-runner/test-runner-pkg.js
+++ karma-test-runner/test-runner-pkg.js
@@ -3,15 +3,19 @@ const { sync } = require("glob");
 function doesPkgHaveATs(packageDirectory) {
   const globOpts = { ignore: [`${packageDirectory}/node_modules/**`] };
 
-  return sync(`${packageDirectory}/**/_test-at/**/*.js`, globOpts).length > 0;
+  const hasTests = sync(`${packageDirectory}/**/_test-at/**/*.js`, globOpts).length > 0;
+  console.log("has ATs?", packageDirectory, hasTests);
+  return hasTests;
 }
 
 module.exports.doesPkgHaveATs = doesPkgHaveATs;
 
 function doesPkgHaveUTs(packageDirectory) {
   const globOpts = { ignore: [`${packageDirectory}/node_modules/**`] };
+  const hasTests = sync(`${packageDirectory}/**/_test-ut/**/*.js`, globOpts).length > 0;
 
-  return sync(`${packageDirectory}/**/_test-ut/**/*.js`, globOpts).length > 0;
+  console.log("has UTs?", packageDirectory, hasTests);
+  return hasTests;
 }
 
 module.exports.doesPkgHaveUTs = doesPkgHaveUTs;
