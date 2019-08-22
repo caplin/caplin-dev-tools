76e7cd1d25dd0d305a339c464ffa81e8f1548b3a
Brian Di Palma <offler@gmail.com>
2019-08-22 08:34:22 +0100

    fix(proxy-factory): Remove spawned process error handling
    This seems to cause persistent e2e test failures in Firefox.

--- express-dev-server/proxy-target-factory.js
+++ express-dev-server/proxy-target-factory.js
@@ -1,9 +1,9 @@
 const { spawn } = require("child_process");
-const logger = require("@caplin/node-logger");
 
 const proxySpawnOptions = {
   // Required for Windows.
-  shell: true
+  shell: true,
+  stdio: "inherit"
 };
 
 module.exports = ({
@@ -15,24 +15,4 @@ module.exports = ({
     "jetty:run",
     "-Djava.util.logging.config.file=server/java/logging.properties"
   ]
-} = {}) => {
-  const process = spawn(cmd, args, proxySpawnOptions);
-
-  process.stderr.on("data", stderrData => {
-    // Convert from `Buffer` to utf-8 string.
-    const errorMessage = stderrData.toString();
-
-    // We can't throw an error here even though it looks like the right thing
-    // to do because sometimes spawned programs log to error output as part of
-    // their standard logging...
-    // eslint-disable-next-line no-console
-    console.error(errorMessage);
-  });
-
-  process.stdout.on("data", stdoutData => {
-    // Convert from `Buffer` to utf-8 string.
-    logger.info(stdoutData.toString());
-  });
-
-  return process;
-};
+} = {}) => spawn(cmd, args, proxySpawnOptions);
